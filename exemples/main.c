8a0c38ffccdd9b5f895774c81d087f90557e255f
Cornichon0097 <antoni.blanche77@gmail.com>
2020-07-30 13:43:59 +0200

    Le main est plus propre

--- exemples/main.c
+++ exemples/main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "../include/couleur.h"
 #include "../include/fenetre.h"
@@ -17,19 +18,22 @@ int main(void)
   Fenetre f = creer_fenetre(X_FENETRE, Y_FENETRE, LARGEUR_FENETRE, HAUTEUR_FENETRE);
 
 
-  if (f)
+  if (f == NULL)
   {
-    afficher_fenetre(f);
-
-    ajouter(f, creer_rectangle(10, 10, 100, 100, ROUGE));
-
-    while (est_ouverte(f))
-    {
-      /* printf("test\n"); */
-    }
-
-    detruire_fenetre(f);
+    fprintf(stderr, "Un problème s'est produit avec la fenêtre. Arrêt.\n");
+    return EXIT_FAILURE;
   }
+
+  afficher_fenetre(f);
+
+  ajouter(f, creer_rectangle(10, 10, 100, 100, ROUGE));
+
+  while (est_ouverte(f))
+  {
+    /* printf("test\n"); */
+  }
+
+  detruire_fenetre(f);
 
 
   return EXIT_SUCCESS;
