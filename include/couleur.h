678643d49152bcdbe1b9841f1e678e6d51e159ac
Cornichon0097 <antoni.blanche77@gmail.com>
2020-08-06 15:23:40 +0200

    Les couleurs de base

--- include/couleur.h
+++ include/couleur.h
@@ -1,7 +1,20 @@
 #ifndef COULEUR_H
 #define COULEUR_H
 
-#define NOIR 0x0
+/* Les couleurs primaires. */
+#define ROUGE   0xff0000
+#define VERT    0x00ff00
+#define BLEU    0x0000ff
+
+/* Les couleurs secondaires. */
+#define JAUNE   0xffff00
+#define MAGENTA 0xff00ff
+#define CYAN    0x00ffff
+
+/* Autres couleurs utiles. */
+#define NOIR    0x0
+#define BLANC   0xffffff
+#define GRIS    0x606060
 
 
 /*
